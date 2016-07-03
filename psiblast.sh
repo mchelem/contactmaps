67f0603e101ec78dcff5ef3ea51bb0a7879e0489
mchelem <michele.silva@gmail.com>
2016-07-03 18:10:10 -0300

    Update database, evalue and # threads for psiblast script

--- psiblast.sh
+++ psiblast.sh
@@ -1,2 +1,2 @@
 wget "http://www.rcsb.org/pdb/download/downloadFile.do?fileFormat=FASTA&compression=NO&structureId=${@}" -O "${@}.fasta"
-psiblast -db blastdb/pdbseqres -out_pssm ${@}.ckp -evalue 0.01 -query ${@}.fasta -out_ascii_pssm ${@}_pssm.txt -out output_file -num_iterations 3
+psiblast -db nr/nr  -evalue 0.001 -query ${@}.fasta -out_ascii_pssm ${@}_pssm.txt -out ${@}.psiblast.output -num_iterations 3 -comp_based_stats 0 -num_threads 8
