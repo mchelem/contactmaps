176812b9c3809745250c4b743ac4ab7c0ce99311
mchelem <michele.silva@gmail.com>
2016-07-03 19:22:15 -0300

    Add DSSP client

---
+++ dssp.py
@@ -0,0 +1,24 @@
+import Bio.PDB
+
+class DsspResult:
+    secondary_structure = None
+    solvent_acessibility = None
+
+    def __repr__(self):
+        "{}\n{}\n".format(
+            self.secondary_structure,
+            self.solvent_accessibility
+        )
+
+
+def assign(filename=None):
+    parser = Bio.PDB.PDBParser()
+    structure = parser.get_structure('X', filename)
+    model = structure[0]
+    dssp = Bio.PDB.DSSP(model, filename)
+    results = [properties[2:4] for properties in dssp.property_list]
+    results = list(zip(*results))
+    dssp_result = DsspResult()
+    dssp_result.secondary_structure = ''.join(results[0])
+    dssp_result.solvent_accessibility = results[1]
+    return dssp_result
